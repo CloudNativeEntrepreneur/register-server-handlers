08bd5a5eae1d8d06f7526e74836600f99b335f2b
Patrick Lee Scott <pat@patscott.io>
2021-07-08 17:06:46 -0500

    feat: cloudevents + serverPath
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- __tests__/unit/index.js
+++ __tests__/unit/index.js
@@ -12,7 +12,33 @@ jest.mock('objectify-folder/modules.js', () => ({
   }))
 }))
 
+jest.mock('server')
+jest.mock('server-cloudevents')
+
 describe('registerHandlers', () => {
+
+  it('should throw an error if misconfigured', async () => {
+    const server = {
+      post: jest.fn()
+    }
+    const path = '/dir'
+    let handlers
+
+    try {
+      handlers = await registerHandlers({
+        server
+      })
+    } catch (err) {
+      expect(err).toEqual(Error('path is required'))
+    }
+    try {
+      handlers = await registerHandlers({
+        path
+      })
+    } catch (err) {
+      expect(err).toEqual(Error('server is required'))
+    }
+  })
   it('should call registerHandlerRoute for each valid module', async () => {
     const server = {
       post: jest.fn()
@@ -22,6 +48,42 @@ describe('registerHandlers', () => {
       server
     })
 
+    const { registerHandlerRoute } = require('server')
+
+    expect(registerHandlerRoute).toBeCalledWith(
+      server, 
+      {
+        handle: expect.any(Function),
+        type: 'example.command'
+      },
+      '/'
+    )
+    expect(handlers['example.command']).toEqual({
+      type: 'example.command',
+      handle: expect.any(Function)
+    })
+  })
+  it('should call registerHandlerRoute from server-cloudevents for each valid module when cloudevents is true', async () => {
+    const server = {
+      post: jest.fn()
+    }
+    const handlers = await registerHandlers({
+      path: '/path/fake/for/tests',
+      server,
+      cloudevents: true,
+      serverPath: '/cloudevent/'
+    })
+
+    const { registerHandlerRoute } = require('server-cloudevents')
+
+    expect(registerHandlerRoute).toBeCalledWith(
+      server, 
+      {
+        handle: expect.any(Function),
+        type: 'example.command'
+      },
+      '/cloudevent/'
+    )
     expect(handlers['example.command']).toEqual({
       type: 'example.command',
       handle: expect.any(Function)
---
+++ __tests__/unit/server-cloudevents.js
@@ -0,0 +1,89 @@
+import { registerHandlerRoute, parseCloudEventForHandler } from 'server-cloudevents.js'
+
+jest.mock('cloudevents', () => ({
+  HTTP: {
+    toEvent: jest.fn(() => {
+      return {
+        type: 'example.commmand',
+        data: {
+          id: 1
+        },
+        source: 'mock'
+      }
+    })
+  }
+}))
+
+describe('server-cloudevents', () => {
+  describe('registerHandlerRoute', () => {
+    it('should register each handler to the server as a post request', () => {
+      const server = {
+        post: jest.fn()
+      }
+      const handler = {
+        type: 'example.command',
+        handle: jest.fn()
+      }
+      registerHandlerRoute(server, handler)
+
+      expect(server.post).toBeCalledWith('/example.command', expect.any(Function))
+    })
+  })
+
+  describe('parseCloudEventForHandler', () => {
+    it('should reply with code 415 if there is an error', () => {
+      const handler = {
+        type: 'example.command',
+        handle: jest.fn()
+      }
+      const req = {
+        headers: {},
+        body: {}
+      }
+      const reply = {
+        code: jest.fn(() => ({
+          header: jest.fn(() => ({
+            send: jest.fn()
+          }))
+        }))
+      }
+
+      const { HTTP } = require('cloudevents')
+      HTTP.toEvent.mockImplementationOnce(() => {
+        throw new Error('blow up')
+      })
+
+      parseCloudEventForHandler(handler)(req, reply)
+
+      expect(handler.handle).not.toBeCalled()
+      expect(reply.code).toBeCalledWith(415)
+    })
+
+    it('should parse the CE and pass along to the handler', () => {
+      const handler = {
+        type: 'example.command',
+        handle: jest.fn()
+      }
+      const req = {
+        headers: {},
+        body: {}
+      }
+      const reply = {
+        code: jest.fn(() => ({
+          header: jest.fn(() => ({
+            send: jest.fn()
+          }))
+        }))
+      }
+      parseCloudEventForHandler(handler)(req, reply)
+
+      expect(handler.handle).toBeCalledWith(expect.any(Object), reply, {
+        type: 'example.commmand',
+        data: {
+          id: 1
+        },
+        source: 'mock'
+      })
+    })
+  })
+})
