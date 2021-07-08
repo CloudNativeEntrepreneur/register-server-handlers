f112a593738627be6a8fe149aa3436748a10e93c
Patrick Lee Scott <pat@patscott.io>
2021-07-08 17:16:24 -0500

    chore: lint + lint-staged
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import debug from 'debug'
 
 const info = debug('register-server-handlers:server')
-const error = debug('register-server-handlers:server:error')
 
 export const registerHandlerRoute = (server, handler, serverPath = '/') => {
   info(`registering route ${serverPath}${handler.type}`)
