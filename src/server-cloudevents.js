718643111f7e560bf9d12cc4937295bbc27cd162
Patrick Lee Scott <pat@patscott.io>
2021-10-06 20:30:08 -0500

    feat: where filter
    Closes #43.
    
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- src/server-cloudevents.js
+++ src/server-cloudevents.js
@@ -6,10 +6,10 @@ const error = debug('register-server-handlers:server-cloudevents:error')
 
 export const registerHandlerRoute = (server, handler, serverPath = '/', handlerOptions = {}) => {
   info(`registering route ${serverPath}${handler.type}`)
-  server.post(`${serverPath}${handler.type}`, parseCloudEventForHandler(handler, handlerOptions))
+  server.post(`${serverPath}${handler.type}`, parseCloudEventThenHandle(handler, handlerOptions))
 }
 
-export const parseCloudEventForHandler = (handler, handlerOptions = {}) => (req, reply) => {
+export const parseCloudEventThenHandle = (handler, handlerOptions = {}) => (req, reply) => {
   info(`parsing cloud event for handler ${handler.type}`)
   try {
     const event = HTTP.toEvent({ headers: req.headers, body: req.body })
@@ -17,7 +17,13 @@ export const parseCloudEventForHandler = (handler, handlerOptions = {}) => (req,
 
     // TODO: feat: filter further via "where" clause for declaritive validation in handlers
 
-    handler.handle(req, reply, event, handlerOptions)
+    if (typeof handler.where === 'function' && !handler.where(event)) {
+      reply.code(400)
+        .header('Content-Type', 'application/json')
+        .send({ err: '🚨 Message does not match where filter criteria' })
+    } else {
+      handler.handle(req, reply, event, handlerOptions)
+    }
   } catch (err) {
     error(err)
     reply.code(415)
